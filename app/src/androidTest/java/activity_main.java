920e26f68e8ba06737325acffe0b114fa9e508ef
Blake Morrow <wmorro4@illinois.edu>
2019-12-02 14:59:22 -0600

    Some past recordings list stuff (unfinished however)

---
+++ app/src/androidTest/java/activity_main.java
@@ -0,0 +1,35 @@
+import android.view.View;
+import android.widget.Button;
+
+import com.example.cs125finalproject.R;
+
+import java.util.ArrayList;
+
+public class activity_main {
+    public ArrayList(ArrayList records) {
+
+    }
+    View recordings = getLayoutInflater().inflate(R.layout.chunk_recordings, records, false);
+
+    Button play - findViewById(R.id.play);
+
+    play.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(set this) {
+            // Do this
+            The code here;
+        }
+    });
+
+    Button delete = findViewById(R.id.delete);
+
+    delete.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(set this) {
+            // Do this
+            The code here;
+        }
+    });
+
+}
+
