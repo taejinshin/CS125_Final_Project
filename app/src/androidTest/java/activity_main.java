069a9faef835365c776b08758ad74396d56c74ef
taejinshin <andrewy5@illinois.edu>
2019-12-02 15:51:33 -0600

    UI somewhat working.

--- app/src/androidTest/java/activity_main.java
+++ app/src/androidTest/java/activity_main.java
@@ -1,35 +1,57 @@
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.cs125finalproject.R;
+import com.example.cs125finalproject.recordinglist;
 
 import java.util.ArrayList;
 
-public class activity_main {
-    public ArrayList(ArrayList records) {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
 
-    }
-    View recordings = getLayoutInflater().inflate(R.layout.chunk_recordings, records, false);
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.os.Handler;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import java.io.IOException;
+import java.lang.Runnable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-    Button play - findViewById(R.id.play);
 
-    play.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(set this) {
-            // Do this
-            The code here;
-        }
-    });
+public class activity_main extends AppCompatActivity {
 
-    Button delete = findViewById(R.id.delete);
-
-    delete.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(set this) {
-            // Do this
-            The code here;
-        }
-    });
 
 }
 
